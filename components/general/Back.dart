6c185e058b6a88b61b54ebfa7c8be97d41c241e3
Caique Gabriel <caiquegabriel@MacBook-Air-de-Caique.local>
2021-06-13 17:14:18 -0300

    ...

--- components/general/Back.dart
+++ components/general/Back.dart
@@ -9,16 +9,34 @@ class Back extends StatelessWidget{
 
   @override 
   Widget build( BuildContext context ){
-    return Row( 
-      children: [
+    return Container( 
+      padding: EdgeInsets.all(3), 
+      decoration: BoxDecoration(
+        color: Colors.yellow[900],
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey,
+            width: 0.6,
+          ),
+          top: BorderSide(
+            color: Colors.grey,
+            width: 0.6,
+          ),
+        ), 
+      ), 
+      child: Row(  
+        children: [
         TextButton(
           child: 
             Row( 
               children: [
-                Icon(Icons.arrow_back),
+                Padding(
+                    child: Icon(Icons.arrow_back, color: Colors.white ),
+                    padding: EdgeInsets.all(5),
+                ),
                 Text( 
                   'Voltar', 
-                  style: TextStyle( fontSize: 22 ),
+                  style: TextStyle( fontSize: 22, color: Colors.white),
                 )
               ],
           ),
@@ -32,6 +50,7 @@ class Back extends StatelessWidget{
           }  
         )
       ],
+      )
     );
   }
 
